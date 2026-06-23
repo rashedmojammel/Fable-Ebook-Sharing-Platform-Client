2295bb6ecb2bb72c05e6cd00899fdbbc5629899f
rashedmojammel <rashedmojamme56@gmail.com>
2026-06-23 21:38:36 +0600

    fix : fixed deploy session issue

--- src/app/dashboard/writer/books/page.jsx
+++ src/app/dashboard/writer/books/page.jsx
@@ -1,4 +1,5 @@
 import { getBooks } from '@/lib/api/job';
+import { getUserSession } from '@/lib/core/session';
 import React from 'react';
 
 const WriterBooks = async() => {
@@ -6,7 +7,8 @@ const WriterBooks = async() => {
 
 
 
-    const userEmail = session?.user?.email ;
+    const user = getUserSession();
+    const userEmail = user?.email; // Extract email from the user session
     const books = await getBooks(userEmail); // Fetch books for the specified email
     console.log("Books:", books); // Log the fetched books to the console
     return (
